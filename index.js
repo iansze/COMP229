8b0dbc5f31f85e32ac993702d4537dfcd9e88855
Ian Sze <iansze1124@gmail.com>
2023-05-13 13:26:41 -0400

    "week1"

---
+++ index.js
@@ -0,0 +1,9 @@
+let http = require("http");
+let dt = require("./date");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+  })
+  .listen(8080);
